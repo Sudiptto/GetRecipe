166b975311d83214bc6a0e1e7e8996683b8db55d
OluwajembolaO <oluwajembolao@nycstudents.net>
2024-03-17 12:23:59 -0400

    Hello World

--- flask_api/YOLO.py
+++ flask_api/YOLO.py
@@ -26,7 +26,7 @@ yolo_logger.addHandler(stream_handler)
 model = YOLO('yolov8n.pt')
 
 # Define path to the image file
-source = 'uploads/pizza.png'
+source = 'uploads/CAKE.png'
 
 # Run inference on the source
 results = model(source)
@@ -39,4 +39,32 @@ model_output = log_buffer.getvalue()
 
 # Now you can process the output as needed
 print()
-print(model_output[110:134])
+print(len(model_output))
+pngString = ".png:"
+speedString = ":deepS"
+frontCounter, backwardCounter = 0 , 0
+l1 , l2, l3 = 0, 0, 0
+r1 = len(model_output)-1
+r2 = len(pngString)-1
+r3 = len(speedString)-1
+while l2 < r2:
+    if model_output[l1] == pngString[l2]:
+        l2 += 1
+    elif model_output[l1] != pngString[l2]:
+        l2 = 0
+    l1 += 1
+    frontCounter += 1
+while l3 < r3:
+    if model_output[r1] == speedString[l3]:
+        l3 += 1
+    elif model_output[l1] != speedString[l2]:
+        l3 = 0
+    r1 -= 1
+    backwardCounter += 1
+print(frontCounter)
+print(backwardCounter)
+    
+
+    
+
+print(model_output[frontCounter + 1: -])
